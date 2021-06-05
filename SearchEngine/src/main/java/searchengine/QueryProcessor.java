3a5fa5d95c3f92c52d1f84723a0538fcb146bc0c
Shorouk Roshdy <shoroukroshdy10@gmail.com>
2021-06-05 15:55:02 +0200

    Non-working query processor

---
+++ SearchEngine/src/main/java/searchengine/QueryProcessor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchengine;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author Shorouk
+ */
+//@SpringBootApplication
+@SpringBootApplication
+public class QueryProcessor {
+    
+    public static void main(String []args)
+    {
+
+         SpringApplication.run(QueryProcessor.class,args);
+    }
+   
+    
+}
